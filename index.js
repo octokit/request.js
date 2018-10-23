bc650b9fba69e2528d925dbc80f3018a20ae64df
Gregor <gregor@martynus.net>
2018-10-22 19:48:08 -0700

    feat: octokitRequest.defaults()

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-module.exports = restRequest
-
-const restEndpoint = require('@octokit/endpoint')
+const endpoint = require('@octokit/endpoint')
 const fetch = require('node-fetch').default
-
-const request = require('./lib/request')
-
 const getUserAgent = require('universal-user-agent')
 
+const request = require('./lib/request')
 const version = require('./package.json').version
 const userAgent = `octokit-request.js/${version} ${getUserAgent()}`
 
-function restRequest (route, options) {
-  const requestOptions = restEndpoint.apply(null, arguments)
-
-  // override the default user-agent header set by @octokit/endpoit
-  if (/^octokit-endpoint.js/.test(requestOptions.headers['user-agent'])) {
-    requestOptions.headers['user-agent'] = userAgent
-  }
-
-  return request(module.exports.fetch, requestOptions)
+function octokitRequest (endpoint, route, options) {
+  return request(module.exports.fetch, endpoint(route, options))
 }
+
+function withDefaults (oldEndpoint, newDefaults) {
+  const endpoint = oldEndpoint.defaults(newDefaults)
+  const request = octokitRequest.bind(null, endpoint)
+  request.endpoint = endpoint
+  request.defaults = withDefaults.bind(null, endpoint)
+  return request
+}
+
+module.exports = withDefaults(endpoint, {
+  headers: {
+    'user-agent': userAgent
+  }
+})
 
 // expose internally used `fetch` method for testing/mocking only
 module.exports.fetch = fetch
