22007068fd2b0edff7722635bb502e4172168623
Gregor Martynus <gregor@martynus.net>
2018-10-30 19:05:08 -0700

    fix: remove private .fetch API (#8)

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 const endpoint = require('@octokit/endpoint')
-const fetch = require('node-fetch').default
 const getUserAgent = require('universal-user-agent')
 
-const request = require('./lib/request')
 const version = require('./package.json').version
 const userAgent = `octokit-request.js/${version} ${getUserAgent()}`
-
-function octokitRequest (endpoint, route, options) {
-  return request(module.exports.fetch, endpoint(route, options))
-}
-
-function withDefaults (oldEndpoint, newDefaults) {
-  const endpoint = oldEndpoint.defaults(newDefaults)
-  const request = octokitRequest.bind(null, endpoint)
-  request.endpoint = endpoint
-  request.defaults = withDefaults.bind(null, endpoint)
-  return request
-}
+const withDefaults = require('./lib/with-defaults')
 
 module.exports = withDefaults(endpoint, {
   headers: {
     'user-agent': userAgent
   }
 })
-
-// expose internally used `fetch` method for testing/mocking only
-module.exports.fetch = fetch
