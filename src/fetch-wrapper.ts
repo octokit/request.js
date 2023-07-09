34303bf08341313d3911706d45ec165a86359213
wolfy1339 <webmaster@wolfy1339.com>
2023-07-09 15:23:50 -0400

    fix: pass through redirect option

--- src/fetch-wrapper.ts
+++ src/fetch-wrapper.ts
@@ -41,6 +41,7 @@ export default function fetchWrapper(
     body: requestOptions.body,
     headers: requestOptions.headers as HeadersInit,
     signal: requestOptions.request?.signal,
+    redirect: requestOptions.redirect,
     // duplex must be set if request.body is ReadableStream or Async Iterables.
     // See https://fetch.spec.whatwg.org/#dom-requestinit-duplex.
     ...(requestOptions.body && { duplex: "half" }),
