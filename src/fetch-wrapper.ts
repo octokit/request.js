053ba746f8b7356aab07092aea6ce78c0a2357ea
uzlopak <aras.abbasi@googlemail.com>
2023-11-21 17:20:58 +0100

    fix import

--- src/fetch-wrapper.ts
+++ src/fetch-wrapper.ts
@@ -1,4 +1,4 @@
-import { isPlainObject } from "is-plain-object";
+import { isPlainObject } from "./is-plain-object";
 import { RequestError } from "@octokit/request-error";
 import type { EndpointInterface } from "@octokit/types";
 
