15a71130d82cb9372e2bfd4276c98ebbe9d2d17e
wolfy1339 <webmaster@wolfy1339.com>
2024-02-26 19:49:41 -0500

    fix(pkg): add `main` export

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -61,6 +61,8 @@ async function main() {
       {
         ...pkg,
         files: ["dist-*/**", "bin/**"],
+        main: "dist-bundle/index.js",
+        types: "dist-types/index.d.ts",
         exports: {
           ".": {
             types: "./dist-types/index.d.ts",
