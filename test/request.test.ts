octokit/request.js

commit ebf08657466aabca9cc57530da1f7856a9a1c09f

feat: v8 (#599)
BREAKING CHANGE: Replace support for Node.js http(s) Agents with documentation on using fetch dispatchers instead

BREAKING CHANGE: Remove ability to pass custom request options, except from `method`, `headers`, `body`, `signal`, `data`

---------

Co-authored-by: Keegan Campbell <me@kfcampbell.com>
Co-authored-by: wolfy1339 <4595477+wolfy1339@users.noreply.github.com>

src/fetch-wrapper.ts
@@ -36,23 +36,16 @@ export default function fetchWrapper(
     );
   }
 
-  return fetch(
-    requestOptions.url,
-    Object.assign(
-      {
-        method: requestOptions.method,
-        body: requestOptions.body,
-        headers: requestOptions.headers as HeadersInit,
-        redirect: requestOptions.redirect,
-        // duplex must be set if request.body is ReadableStream or Async Iterables.
-        // See https://fetch.spec.whatwg.org/#dom-requestinit-duplex.
-        ...(requestOptions.body && { duplex: "half" }),
-      },
-      // `requestOptions.request.agent` type is incompatible
-      // see https://github.com/octokit/types.ts/pull/264
-      requestOptions.request as any,
-    ),
-  )
+  return fetch(requestOptions.url, {
+    method: requestOptions.method,
+    body: requestOptions.body,
+    headers: requestOptions.headers as HeadersInit,
+    signal: (requestOptions as any).signal,
+    data: (requestOptions as any).data,
+    // duplex must be set if request.body is ReadableStream or Async Iterables.
+    // See https://fetch.spec.whatwg.org/#dom-requestinit-duplex.
+    ...(requestOptions.body && { duplex: "half" }),
+  })
     .then(async (response) => {
       url = response.url;
       status = response.status;

test/request.test.ts
@@ -561,25 +561,6 @@ x//0u+zd/R/QRUzLOw4N72/Hu+UG6MNt5iDZFCtapRaKt6OvSBwy8w==
     );
   });
 
-  it("options.request.signal is passed as option to fetch", function () {
-    return request("/", {
-      request: {
-        // We pass a value that is not an `AbortSignal`, and expect `fetch` to
-        // throw an exception complaining about the value
-        signal: "funk",
-      },
-    })
-      .then(() => {
-        throw new Error("Should not resolve");
-      })
-
-      .catch((error) => {
-        // We can't match on the entire string because the message differs between
-        // Node versions.
-        expect(error.message).toMatch(/AbortSignal/);
-      });
-  });
-
   it("options.request.fetch", function () {
     return request("/", {
       request: {

commit 23200c03557029a615e026376c93ec33ce63f7ff

feat: re-add `redirect` request option (#636)
* Fixed regression of redirect option being passed through to fetch introduced in #599, and moves it instead to the `requestOption.request` object

* build: upgrade jest to fix test regression with `globalThis` being read-only

src/fetch-wrapper.ts
@@ -39,6 +39,7 @@ export default function fetchWrapper(
   return fetch(requestOptions.url, {
     method: requestOptions.method,
     body: requestOptions.body,
+    redirect: requestOptions.request?.redirect,
     // Header values must be `string`
     headers: Object.fromEntries(
       Object.entries(requestOptions.headers).map(([name, value]) => [

test/request.test.ts
@@ -1107,4 +1107,18 @@ x//0u+zd/R/QRUzLOw4N72/Hu+UG6MNt5iDZFCtapRaKt6OvSBwy8w==
       expect(mock.done()).toBe(true);
     });
   });
+
+  it("request should pass the `redirect` option to fetch", () => {
+    const customFetch = async (url: string, options: RequestInit) => {
+      expect(options.redirect).toEqual("manual");
+      return await fetch(url, options);
+    };
+
+    return request("/", {
+      request: {
+        redirect: "manual",
+        fetch: customFetch,
+      },
+    });
+  });
 });